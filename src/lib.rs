e470d88484648355032f7ef327e624583e800422
Daniel Stapleton <blamblamdan@gmail.com>
2024-02-12 18:48:51 +0000

    Formatted code to pass automated scripts.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,12 @@
 use once_cell::sync::Lazy;
 use regex::{Captures, Regex};
 use std::{
-    io::{self, BufRead, ErrorKind::InvalidInput}, 
-    borrow::Cow, 
-    num::ParseIntError
+    borrow::Cow,
+    io::{self, BufRead, ErrorKind::InvalidInput},
+    num::ParseIntError,
 };
 
-static REGEX: Lazy<Regex> = Lazy::new(|| 
-    regex::Regex::new(r"\b(0x)?([0-9a-fA-F]{2,})\b").unwrap()
-);
+static REGEX: Lazy<Regex> = Lazy::new(|| regex::Regex::new(r"\b(0x)?([0-9a-fA-F]{2,})\b").unwrap());
 // Match at compile time
 const NEWLINE: &str = if cfg!(windows) { "\r\n" } else { "\n" };
 
@@ -19,7 +17,7 @@ const NEWLINE: &str = if cfg!(windows) { "\r\n" } else { "\n" };
 /// # Errors
 /// This function will return an error when `replacement` fails on a capture.
 /// # Examples
-/// See examples in [`regex::Regex::replace_all`]. 
+/// See examples in [`regex::Regex::replace_all`].
 fn replace_all<'h, E>(
     re: &Regex,
     haystack: &'h str,
@@ -42,17 +40,19 @@ fn replace_all<'h, E>(
 /// Option to return early with [`Ok`] if a blank line (`"\r"` on Unix-like and `"\r\n"`
 /// on Windows-like) is provided.
 /// # Errors
-/// This function errors if a line is failed to be read from STDIN (See 
-/// [`io::BufRead::read_line`]) or if [`hex2dec_line`] fails on a line. 
+/// This function errors if a line is failed to be read from STDIN (See
+/// [`io::BufRead::read_line`]) or if [`hex2dec_line`] fails on a line.
 /// If `skip_parse_errors` is set to `true` then `stop_on_error` will be ignored.
 /// # Future
 /// API may be changed so that this function returns any values produced by `f`.
 /// Alternatively a secondary function may be provided.
 pub fn parse_stdin<E: Fn(ParseIntError) -> io::Error, F: Fn(String)>(
-    ok_callback: F, error_callback: E, 
-    skip_parse_errors: bool, stop_on_error: bool, 
-    break_nl: bool
-) -> Result<(),io::Error>{
+    ok_callback: F,
+    error_callback: E,
+    skip_parse_errors: bool,
+    stop_on_error: bool,
+    break_nl: bool,
+) -> Result<(), io::Error> {
     let mut line = String::new();
     let mut handle = io::stdin().lock();
 
@@ -61,12 +61,19 @@ pub fn parse_stdin<E: Fn(ParseIntError) -> io::Error, F: Fn(String)>(
 
         // Break if EOF
         // or a blank line is reached with break_newline true.
-        if nbytes == 0 || (break_nl && (line == NEWLINE)) { return Ok(()); }
+        if nbytes == 0 || (break_nl && (line == NEWLINE)) {
+            return Ok(());
+        }
 
         match hex2dec_line(&line, skip_parse_errors) {
             Ok(s) => ok_callback(s),
-            Err(e) => if stop_on_error { return Err(error_callback(e))} 
-            else { error_callback(e); }
+            Err(e) => {
+                if stop_on_error {
+                    return Err(error_callback(e));
+                } else {
+                    error_callback(e);
+                }
+            }
         };
 
         line.clear();
@@ -76,13 +83,19 @@ pub fn parse_stdin<E: Fn(ParseIntError) -> io::Error, F: Fn(String)>(
 /// Wrapper for [`parse_stdin`] for usage with CI utilities.
 /// Errors are redirected to STDERR for compatibility with CI operations.
 pub fn parse_ci<E: Fn(ParseIntError) -> io::Error, F: Fn(String)>(
-    ok_callback: F, error_callback: E, 
-    skip_parse_errors: bool, stop_on_error: bool,
-    break_nl: bool
-){
+    ok_callback: F,
+    error_callback: E,
+    skip_parse_errors: bool,
+    stop_on_error: bool,
+    break_nl: bool,
+) {
     if let Err(e) = parse_stdin(
-        ok_callback, error_callback, skip_parse_errors, stop_on_error, break_nl
-    ) { 
+        ok_callback,
+        error_callback,
+        skip_parse_errors,
+        stop_on_error,
+        break_nl,
+    ) {
         match e.kind() {
             InvalidInput => eprint!("An error occured in hex2dec_line. {}", e),
             _ => eprint!("An error occured in parse_stdin. {}", e),
@@ -100,34 +113,28 @@ pub fn parse_ci<E: Fn(ParseIntError) -> io::Error, F: Fn(String)>(
 /// use hex2dec::hex2dec_line;
 /// let s = hex2dec_line("0x5a200", false);
 /// assert_eq!(s.unwrap(), " 369152"); // String is padded to the same length
-/// ``` 
-pub fn hex2dec_line<S: AsRef<str>>(line: S, skip_error: bool)
- -> Result<String, std::num::ParseIntError>{
-    let new_str = replace_all(
-        &REGEX, line.as_ref(),
-        |caps: &Captures| {
-            let m = caps.get(0).unwrap();
-            let format_length = m.len();
-            let hex_str = caps.get(2).unwrap().as_str();
+/// ```
+pub fn hex2dec_line<S: AsRef<str>>(
+    line: S,
+    skip_error: bool,
+) -> Result<String, std::num::ParseIntError> {
+    let new_str = replace_all(&REGEX, line.as_ref(), |caps: &Captures| {
+        let m = caps.get(0).unwrap();
+        let format_length = m.len();
+        let hex_str = caps.get(2).unwrap().as_str();
 
-            let dec = if skip_error {
-                match u128::from_str_radix(hex_str, 16) {
-                    Ok(d) => d,
-                    Err(_) => return Ok(m.as_str().to_owned()) // Use original substring
-                }
-            } else {
-                u128::from_str_radix(hex_str, 16)?
-            };
-            
-            Ok(
-                format!(
-                    "{:width$}",
-                    dec,
-                    width = format_length
-                )
-            )
-        })?
-        .into_owned();
+        let dec = if skip_error {
+            match u128::from_str_radix(hex_str, 16) {
+                Ok(d) => d,
+                Err(_) => return Ok(m.as_str().to_owned()), // Use original substring
+            }
+        } else {
+            u128::from_str_radix(hex_str, 16)?
+        };
+
+        Ok(format!("{:width$}", dec, width = format_length))
+    })?
+    .into_owned();
 
     Ok(new_str)
 }
@@ -137,7 +144,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_hex2dec_line(){
+    fn test_hex2dec_line() {
         let tests = [
             ("", ""),
             (" a ", " a "),
@@ -159,7 +166,7 @@ mod tests {
             assert_eq!(hex2dec_line(test.0, false).unwrap(), test.1);
         }
     }
-    
+
     // #[test]
     // fn stdin() -> Result<(), io::Error>{
     //     parse_stdin(|s| println!("{}", s))?;
--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,7 @@ use std::io;
 
 fn main() {
     let callback = |s| println!("{}", s);
-    let on_error = |e| {
-        io::Error::new(io::ErrorKind::InvalidInput, e)
-    };
+    let on_error = |e| io::Error::new(io::ErrorKind::InvalidInput, e);
     // (bool, bool, bool) <-> (skip_parse_errors, stop_on_error, break_nl)
     parse_ci(callback, on_error, false, true, false);
 }
